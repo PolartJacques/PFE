8b3ddd1fcfe20994bb4998413640f225194ba6e2
Jacques <jack.polart@gmail.com>
2022-11-15 16:41:09 +0100

    feature: clean full script

--- client-app/public/script.js
+++ client-app/public/script.js
@@ -1,56 +1,101 @@
-console.log("hello world")
+const ONE_MINUTE_IN_MLS = 60000
+const FIVE_MINUTE_IN_MLS = 5 * ONE_MINUTE_IN_MLS
+
 const data = {
-	action: "",
 	ip: "",
 	origin: "",
-	connexionTime: Date.now(),
-	endOfSession: 0
+	userId: ""
 }
 
-// get origin
-const urlParams = new URLSearchParams(window.location.search)
-const origin = urlParams.get("origin")
-data.origin = origin ?? ""
-console.log(data)
-
-// get ip address
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(response => {
-		data.ip = response.ip
-		console.log(data)
+const uuidv4 = () => {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+			const r = Math.random() * 16 | 0, v = c === 'x' ? r : ((r & 0x3) | 0x8)
+			return v.toString(16)
 	})
+}
 
-// window.addEventListener('beforeunload',  event => {
-// 	data.endOfSession = Date.now()
-// 	console.log(data)
-// 	event.returnValue = ""
-// })
+const getUserId = () => {
+	const userId = localStorage.getItem('userId')
+	if (Boolean(userId)) return userId
+	const newUserId = uuidv4()
+	localStorage.setItem('userId', newUserId)
+	return newUserId
+}
 
+const getIpAddress = async () => {
+	const fullIp = (await (await fetch('https://api.ipify.org?format=json')).json()).ip
+	const hiddenIp = fullIp.split(".")
+	hiddenIp[2] = 0
+	hiddenIp[3] = 0
+	return hiddenIp.join(".")
+}
 
-// detect location changes
-window.history.pushState = ( f => function pushState(){
-	const ret = f.apply(this, arguments)
-	window.dispatchEvent(new Event('locationchange'))
-	return ret;
-})(window.history.pushState)
+const getOriginCookie = () => {
+  const value = `; ${document.cookie}`
+  const parts = value.split(`; origin=`)
+  if (parts.length === 2) return parts.pop().split(';').shift()
+	return null
+}
 
-window.history.replaceState = ( f => function replaceState(){
-	const ret = f.apply(this, arguments)
-	window.dispatchEvent(new Event('locationchange'))
-	return ret;
-})(window.history.replaceState)
+const getOrigin = () => {
+	const urlParams = new URLSearchParams(window.location.search)
+	const originFromUrl = urlParams.get("origin")
+	if (Boolean(originFromUrl)) return originFromUrl
+	const originFromCookies = getOriginCookie()
+	if (Boolean(originFromCookies)) return originFromCookies
+	return ""
+}
 
-window.addEventListener('popstate', () => {
-	window.dispatchEvent(new Event('locationchange'))
-});
+const fetchContextData = async () => {
+	data.ip = await getIpAddress()
+	data.origin = getOrigin()
+	data.userId = getUserId()
+}
 
-window.addEventListener('locationchange', event => {
-	console.log('locationchange')
-	console.log(event.currentTarget.location)
-})
+const saveOriginInCookies = () => {
+	let expirationDate = new Date(Date.now() + FIVE_MINUTE_IN_MLS)
+	document.cookie = `origin=${data.origin};expires=${expirationDate.toUTCString()}`
+}
 
-// detect custom event
-window.addEventListener('myCustomEvent', event => {
-	console.log('custom event : ', event.detail.name)
-})
+const sendAction = (action) => {
+	console.log({ ...data, action, date: Date.now(), location: window.location.href})
+}
+
+const listenToLocationChange = () => {
+	window.history.pushState = ( f => function pushState(){
+		const ret = f.apply(this, arguments)
+		window.dispatchEvent(new Event('locationchange'))
+		return ret
+	})(window.history.pushState)
+	
+	window.history.replaceState = ( f => function replaceState(){
+		const ret = f.apply(this, arguments)
+		window.dispatchEvent(new Event('locationchange'))
+		return ret
+	})(window.history.replaceState)
+	
+	window.addEventListener('popstate', () => {
+		window.dispatchEvent(new Event('locationchange'))
+	})
+	
+	window.addEventListener('locationchange', () => {
+		sendAction('location changed')
+	})
+}
+
+const listenToCustomEvent = () => {
+	window.addEventListener('myCustomEvent', event => {
+		sendAction(event.detail.name)
+	})
+}
+
+const main = async () => {
+	await fetchContextData()
+	saveOriginInCookies()
+	sendAction("visit or reload")
+
+	listenToLocationChange()
+	listenToCustomEvent()
+}
+
+main()
